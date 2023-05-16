305fbe5904c9f7ff731e7deaf808d542b70ab8e9
joao <99joaomartins@gmail.com>
2023-05-16 11:36:22 +0100

    Tutorial completed

---
+++ movie-catalog-service/src/main/java/io/marionete/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,39 @@
+package io.marionete.moviecatalogservice.resources;
+
+import com.netflix.discovery.DiscoveryClient;
+import io.marionete.moviecatalogservice.models.CatalogItem;
+import io.marionete.moviecatalogservice.models.Movie;
+import io.marionete.moviecatalogservice.models.Rating;
+import io.marionete.moviecatalogservice.models.UserRating;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
+
+        return ratings.getUserRating().stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(),"Desc", rating.getRating());
+        })
+        .collect(Collectors.toList());
+
+    }
+}
